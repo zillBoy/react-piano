fc51454fe6c2c1fd93e9b81f853c434d2e51aebc
Muhammad Usman <musman0741@gmail.com>
2023-04-26 17:43:12 +0500

    [feature/footer-logo-component] App component styles applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 // Internal Dependencies
-import { Footer } from './components/Footer'
-import { Logo } from './components/Logo/Logo';
+import { Footer } from "./components/Footer";
+import { Logo } from "./components/Logo/Logo";
 
-import styles from './App.module.css'
+import styles from "./App.module.css";
 
 export const App = () => {
-  return <div>
-    <Logo />
-    <main className={styles.content} />
-    <Footer />
-  </div>
-}
+  return (
+    <div className={styles.app}>
+      <Logo />
+      <main className={styles.content}>
+        <p>Helo</p>
+      </main>
+      <Footer />
+    </div>
+  );
+};
