16c324c7a458cab89a0f49edb58cf274bb93bd67
Muhammad Usman <41048737+zillBoy@users.noreply.github.com>
2023-04-27 17:58:40 +0500

    Feature/music domain notes (#3)
    * [feature/music-domain-notes] domain types created
    
    * [feature/music-domain-notes] remove unused note variable value
    
    * [feature/music-domain-notes] access browser AudioAPI accessContext function and type created
    
    * [feature/music-domain-notes] code structured improved

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,12 +1,18 @@
 // Internal Dependencies
-import styles from './Logo.module.css'
+import styles from "./Logo.module.css";
 
 export const Logo = () => {
   return (
     <h1 className={styles.logo}>
-      <span role='img' aria-label='metal hand emoji'>🤘</span>
-      <span role='img' aria-label='musical keyboard emoji'>🎹</span>
-      <span role='img' aria-label='musical notes emoji'>🎵</span>
+      <span role="img" aria-label="metal hand emoji">
+        🤘
+      </span>
+      <span role="img" aria-label="musical keyboard emoji">
+        🎹
+      </span>
+      <span role="img" aria-label="musical notes emoji">
+        🎵
+      </span>
     </h1>
-  )
-}
+  );
+};
