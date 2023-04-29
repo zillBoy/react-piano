d106d4a34243d8b22e2edf5cfc8dd63e59956f4d
Muhammad Usman <musman0741@gmail.com>
2023-04-29 18:39:27 +0500

    [feature/keyboard-connect] keypress implemented

--- src/components/Key/Key.tsx
+++ src/components/Key/Key.tsx
@@ -6,6 +6,7 @@ import clsx from "clsx";
 
 // Internal Dependencies
 import { NoteType } from "../../domain/note";
+import { usePressObserver } from "../PressObserver";
 
 import styles from "./Key.module.css";
 
@@ -22,14 +23,23 @@ type KeyProps = {
 
 export const Key: React.FC<KeyProps> = (props) => {
   const { type, label, onDown, onUp, ...rest } = props;
+  const pressed = usePressObserver({
+    watchKey: label,
+    onStartPress: onDown,
+    onFinishPress: onUp,
+  });
 
   return (
     <button
-      className={clsx(styles.key, styles[type])}
-      type="button"
-      {...rest}
+      className={clsx(
+        styles.key,
+        styles[type],
+        pressed && styles["is-pressed"]
+      )}
       onMouseDown={onDown}
       onMouseUp={onUp}
+      type="button"
+      {...rest}
     >
       {label}
     </button>
